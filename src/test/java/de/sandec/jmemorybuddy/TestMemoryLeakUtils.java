0cd9d542faa9049f3f9c82bc0cbfa8f3ffb92b09
floriankirmaier <florian.kirmaier@gmail.com>
2019-11-04 22:25:57 +0100

    added unit-test, added publish to sonatype

--- src/test/java/de/sandec/jmemorybuddy/TestMemoryLeakUtils.java
+++ src/test/java/de/sandec/jmemorybuddy/TestMemoryLeakUtils.java
@@ -1,4 +1,36 @@
 package de.sandec.jmemorybuddy;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMemoryLeakUtils {
+
+    class A {
+
+    }
+
+    @Test
+    public void simpleTest() {
+        A referenced = new A();
+        MemoryLeakUtils.doMemTest(checker -> {
+            A notReferenced = new A();
+            checker.accept(notReferenced); // not referenced should be collectable
+        });
+    }
+
+    @Test
+    public void negativeTest() {
+        boolean exceptionThrown = false;
+        try {
+            A referenced = new A();
+            MemoryLeakUtils.doMemTest(checker -> {
+                checker.accept(referenced);
+            });
+        } catch (Exception e) {
+            exceptionThrown = true;
+        }
+        Assert.assertTrue(exceptionThrown);
+    }
+
 }
