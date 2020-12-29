8e536e9bcadfee687467ac7321f56f8102cbb9b0
floriankirmaier <florian.kirmaier@gmail.com>
2020-12-29 18:36:07 +0100

    fixed race condition in unit test

--- src/test/java/de/sandec/jmemorybuddy/TestJMemoryBuddyLive.java
+++ src/test/java/de/sandec/jmemorybuddy/TestJMemoryBuddyLive.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class TestJMemoryBuddyLive {
     @Test
-    public void testMarkKeepsGCBehaviour() {
+    public void testMarkKeepsGCBehaviour() throws InterruptedException {
 
         int initialCollected = JMemoryBuddyLive.getReport().collectedEntries;
         JMemoryBuddy.memoryTest(checker -> {
@@ -16,6 +16,8 @@ public class TestJMemoryBuddyLive {
             JMemoryBuddyLive.markCollectable("test", o);
             checker.assertCollectable(o);
         });
+        Thread.sleep(50); // we might have a slight delay,
+                                   // because processing the collection might happen slightly delayed.
         Assert.assertEquals(initialCollected + 1, JMemoryBuddyLive.getReport().collectedEntries);
     }
 }
