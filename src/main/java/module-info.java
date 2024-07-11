59de4ada81dcbe8aafcc95f9440d1c40699607d1
Indrit Beqiri <ib@sandec.de>
2024-07-11 14:28:07 +0200

    Modularise `de.sandec.jmemorybuddy` module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * Module descriptor for the JMemoryBuddy module.
+ */
+module de.sandec.jmemorybuddy {
+    requires java.management;
+    requires jdk.management;
+
+    exports de.sandec.jmemorybuddy;
+}
